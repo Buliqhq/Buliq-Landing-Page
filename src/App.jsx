b0987cd1b942697d46970a36fd80439b65d87051
nasihudeen04@gmail.com <nasihudeen04@gmail.com>
2025-01-22 18:05:10 +0100

    contact section merged

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 
-import React from "react";
 import Contact from "./components/Contact"
+import Navbar from "./components/NavBar"
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
+      <Navbar/>
+      <main>
       <Contact />
+      </main>
+      
+      </div>
+  )
+}
 
 export default App
