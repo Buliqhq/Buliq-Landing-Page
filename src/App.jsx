13aad82e69fee468da5aa99df7ba6cba77cc79bb
nasihudeen04@gmail.com <nasihudeen04@gmail.com>
2025-01-24 21:25:25 +0100

    conflicts solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,89 +1,27 @@
-
 import React from "react";
 import Contact from "./components/Contact";
 import Feature from "./components/Feature";
 
-
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-import HeroSection from "./components/HeroSection"
-import Navbar from "./components/NavBar"
-import ValueSection from "./components/ValueSection"
-
+import Footer from "./components/Footer";
+import HeroSection from "./components/HeroSection";
+import Navbar from "./components/NavBar";
+import ValueSection from "./components/ValueSection";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
-      <Navbar/>
+      <Navbar />
       <main>
-        <HeroSection/>
-        <ValueSection/>
-      <Contact />
+        <HeroSection />
+        <ValueSection />
+        <Contact />
 
-      <Feature />
-    </div>
-
+        <Feature />
       </main>
-      <Footer/>
-
-      
-      </div>
-  )
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
 
- {/* Cards Section */}
-        {/* <div className="text-center">
-          <h3 className="text-2xl lg:text-3xl font-bold text-white mb-8">
-            The Ultimate Layer for Asset Re:allocation
-          </h3>
-          <p className="text-white max-w-2xl mx-auto mb-12">
-            Uncover Your gateway to smarter, more rewarding asset
-            reallocation.
-          </p>
-        </div> */}
-
-        {/* Cards */}
-        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
-         
-          <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
-            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
-              Liquid Staking TVL
-            </h4>
-            <div className="h-[100px] flex justify-center items-center">
-              <img
-                src="/path-to-icon" 
-                alt="Liquid Staking"
-                className="h-[60px]"
-              />
-            </div>
-          </div> */}
-
-          {/* Card 2 */}
-          {/* <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
-            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
-              DEX Usage
-            </h4>
-            <div className="h-[100px] flex justify-center items-center">
-              <img
-                src="/path-to-icon" // Replace with your icon's path
-                alt="DEX Usage"
-                className="h-[60px]"
-              />
-            </div>
-          </div> */}
-
-          {/* Card 3 */}
-          {/* <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
-            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
-              L2 Usage & TVL
-            </h4>
-            <div className="h-[100px] flex justify-center items-center">
-              <img
-                src="/path-to-icon" // Replace with your icon's path
-                alt="L2 Usage"
-                className="h-[60px]"
-              />
-            </div>
-          </div> */}
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,3 @@
-import { ArrowRight } from "lucide-react";
 
 const HeroSection = () => {
   return (
@@ -18,10 +17,25 @@ const HeroSection = () => {
             Claim Now
           </div>
 
-          <button className="flex items-center space-x-2 bg-[#1E2959] text-white px-6 py-2 rounded-full hover:bg-[#161d3e] transition-colors">
-            <span>Claim Point</span>
-            <ArrowRight className="h-5 w-5" />
-          </button>
+          <button
+              className="flex items-center justify-center font-semibold shadow-md bg-[#16345A] text-[#F5F5F5] rounded-full px-6 py-3 space-x-2 hover:bg-opacity-80 transition-colors duration-300 mx-auto lg:mx-0"
+            >
+              <span>Sign up for waitlists</span>
+              <svg
+                xmlns="http://www.w3.org/2003/svg"
+                viewBox="0 0 25 25"
+                fill="none"
+                className="w-6 h-6"
+              >
+                <path
+                  d="M12.5 16.2698L16.5 12.2698M16.5 12.2698L12.5 8.26978M16.5 12.2698H8.5M22.5 12.2698C22.5 17.7926 18.0228 22.2698 12.5 22.2698C6.97715 22.2698 2.5 17.7926 2.5 12.2698C2.5 6.74693 6.97715 2.26978 12.5 2.26978C18.0228 2.26978 22.5 6.74693 22.5 12.2698Z"
+                  stroke="#F5F5F5"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+            </button>
         </div>
       </div>
 
