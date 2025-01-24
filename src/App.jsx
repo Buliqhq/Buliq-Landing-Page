1c5eeb8f9a44f1e9aa1fb52d80c9d42aada4372f
khaddyBola <khadijahmusa232@gmail.com>
2025-01-24 18:23:37 +0100

    feature section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,69 @@
 import React from "react";
-import Contact from "./components/Contact"
+import Contact from "./components/Contact";
+import Feature from "./components/Feature";
 
 function App() {
   return (
     <div className="min-h-screen bg-white">
       <Contact />
+      <Feature />
     </div>
   )
 }
 
 export default App
+
+ {/* Cards Section */}
+        {/* <div className="text-center">
+          <h3 className="text-2xl lg:text-3xl font-bold text-white mb-8">
+            The Ultimate Layer for Asset Re:allocation
+          </h3>
+          <p className="text-white max-w-2xl mx-auto mb-12">
+            Uncover Your gateway to smarter, more rewarding asset
+            reallocation.
+          </p>
+        </div> */}
+
+        {/* Cards */}
+        {/* <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 px-6">
+         
+          <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
+            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
+              Liquid Staking TVL
+            </h4>
+            <div className="h-[100px] flex justify-center items-center">
+              <img
+                src="/path-to-icon" 
+                alt="Liquid Staking"
+                className="h-[60px]"
+              />
+            </div>
+          </div> */}
+
+          {/* Card 2 */}
+          {/* <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
+            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
+              DEX Usage
+            </h4>
+            <div className="h-[100px] flex justify-center items-center">
+              <img
+                src="/path-to-icon" // Replace with your icon's path
+                alt="DEX Usage"
+                className="h-[60px]"
+              />
+            </div>
+          </div> */}
+
+          {/* Card 3 */}
+          {/* <div className="bg-[#CFF6FE] p-6 rounded-lg shadow-md text-center">
+            <h4 className="text-xl font-semibold text-[#0A2742] mb-4">
+              L2 Usage & TVL
+            </h4>
+            <div className="h-[100px] flex justify-center items-center">
+              <img
+                src="/path-to-icon" // Replace with your icon's path
+                alt="L2 Usage"
+                className="h-[60px]"
+              />
+            </div>
+          </div> */}
