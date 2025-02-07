55262f5051d2b8b58766642eed62d0ab7d659738
khaddyBola <khadijahmusa232@gmail.com>
2025-02-07 20:45:01 +0100

    final-changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,20 +18,6 @@ import {  Menu, X } from "lucide-react"
               className="flex items-center justify-center font-semibold shadow-md bg-[#16345A] text-[#F5F5F5] rounded-full px-6 py-3 space-x-2 hover:bg-opacity-80 transition-colors duration-300 mx-auto lg:mx-0"
             >
               <span>Sign up for waitlists</span>
-              <svg
-                xmlns="http://www.w3.org/2003/svg"
-                viewBox="0 0 25 25"
-                fill="none"
-                className="w-6 h-6"
-              >
-                <path
-                  d="M12.5 16.2698L16.5 12.2698M16.5 12.2698L12.5 8.26978M16.5 12.2698H8.5M22.5 12.2698C22.5 17.7926 18.0228 22.2698 12.5 22.2698C6.97715 22.2698 2.5 17.7926 2.5 12.2698C2.5 6.74693 6.97715 2.26978 12.5 2.26978C18.0228 2.26978 22.5 6.74693 22.5 12.2698Z"
-                  stroke="#F5F5F5"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
             </button>
           </div>
 
